b8ad3b0cb1580f25e495fb48bc4f810d286d489b
ketlller <oculusvrdk2@gmail.com>
2019-12-09 23:47:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ var divSell = document.querySelector('.sell span');
 var divBuyPZM_BTC = document.querySelector('.buyPZM_BTC span');
 var divSellPZM_BTC = document.querySelector('.sellPZM_BTC span');
 
-var divBuyPZM_USD = document.querySelector('.buyPZM_USD span');
-var divSellPZM_USD = document.querySelector('.sellPZM_USD span');
-var divBuyPZM_USD_livecoin = document.querySelector('.buyPZM_USD_livecoin span');
-var divSellPZM_USD_livecoin = document.querySelector('.sellPZM_USD_livecoin span');
-var divBuyPZM_USD_hotbit = document.querySelector('.buyPZM_USD_hotbit span');
-var divSellPZM_USD_hotbit = document.querySelector('.sellPZM_USD_hotbit span');
+var divBuyCOIN_USD = document.querySelector('.buyCOIN_USD span');
+var divSellCOIN_USD = document.querySelector('.sellCOIN_USD span');
+var divBuyCOIN_USD_livecoin = document.querySelector('.buyCOIN_USD_livecoin span');
+var divSellCOIN_USD_livecoin = document.querySelector('.sellCOIN_USD_livecoin span');
+var divBuyCOIN_USD_hotbit = document.querySelector('.buyCOIN_USD_hotbit span');
+var divSellCOIN_USD_hotbit = document.querySelector('.sellCOIN_USD_hotbit span');
 
-var divBuyPZM_USD_livecoin1 = document.querySelector('.BuyPZM_USD_livecoin1 span');
-var divSellPZM_USD_livecoin1 = document.querySelector('.SellPZM_USD_livecoin1 span');
-var divBuyPZM_USD1 = document.querySelector('.BuyPZM_USD1 span');
-var divSellPZM_USD1 = document.querySelector('.SellPZM_USD1 span');
-var divBuyPZM_USD_hotbit1 = document.querySelector('.BuyPZM_USD_hotbit1 span');
-var divSellPZM_USD_hotbit1 = document.querySelector('.SellPZM_USD_hotbit1 span');
+var divBuyCOIN_USD_livecoin1 = document.querySelector('.BuyCOIN_USD_livecoin1 span');
+var divSellCOIN_USD_livecoin1 = document.querySelector('.SellCOIN_USD_livecoin1 span');
+var divBuyCOIN_USD1 = document.querySelector('.BuyCOIN_USD1 span');
+var divSellCOIN_USD1 = document.querySelector('.SellCOIN_USD1 span');
+var divBuyCOIN_USD_hotbit1 = document.querySelector('.BuyCOIN_USD_hotbit1 span');
+var divSellCOIN_USD_hotbit1 = document.querySelector('.SellCOIN_USD_hotbit1 span');
  
 var btcalphalivecoin = document.querySelector('.inputdrub1 span');
 var livecoinbtcalpha = document.querySelector('.inputdrub2 span');
@@ -30,26 +30,26 @@ var livecoinhotbit = document.querySelector('.inputdrub6 span');
 
 let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
 
-    targetUrl_PZM_USD = 'https://btc-alpha.com/api/v1/orderbook/PZM_USD/'; 
-    targetUrl_PZM_USD_Livecoin = 'https://api.livecoin.net/exchange/order_book?currencyPair=PZM/USD'; 
-      targetUrl_PZM_USD_hotbit1 = 'https://api.hotbit.io/api/v1/order.book?market=PZM/USDT&side=1&offset=0&limit=1'; 
-      targetUrl_PZM_USD_hotbit2 = 'https://api.hotbit.io/api/v1/order.book?market=PZM/USDT&side=2&offset=0&limit=1'; 
+    targetUrl_COIN_USD = 'https://btc-alpha.com/api/v1/orderbook/PZM_USD/'; 
+    targetUrl_COIN_USD_Livecoin = 'https://api.livecoin.net/exchange/order_book?currencyPair=PZM/USD'; 
+      targetUrl_COIN_USD_hotbit1 = 'https://api.hotbit.io/api/v1/order.book?market=PZM/USDT&side=1&offset=0&limit=1'; 
+      targetUrl_COIN_USD_hotbit2 = 'https://api.hotbit.io/api/v1/order.book?market=PZM/USDT&side=2&offset=0&limit=1'; 
 
 
     
-let resUrl1 = proxyUrl + targetUrl_PZM_USD;
-let resUrl2 = proxyUrl + targetUrl_PZM_USD_Livecoin;
-let resUrl3 = proxyUrl + targetUrl_PZM_USD_hotbit1;
-let resUrl4 = proxyUrl + targetUrl_PZM_USD_hotbit2;
+let resUrl1 = proxyUrl + targetUrl_COIN_USD;
+let resUrl2 = proxyUrl + targetUrl_COIN_USD_Livecoin;
+let resUrl3 = proxyUrl + targetUrl_COIN_USD_hotbit1;
+let resUrl4 = proxyUrl + targetUrl_COIN_USD_hotbit2;
 
 console.log()
 
- var currentPrice_BuyPZM_USD = null;
- var currentPrice_SellPZM_USD = null;
-  var currentPrice_BuyPZM_USD_livecoin = null;
-  var currentPrice_SellPZM_USD_livecoin = null;
-var currentPrice_BuyPZM_USD_hotbit = null;
-var currentPrice_SellPZM_USD_hotbit = null;
+ var currentPrice_BuyCOIN_USD = null;
+ var currentPrice_SellCOIN_USD = null;
+  var currentPrice_BuyCOIN_USD_livecoin = null;
+  var currentPrice_SellCOIN_USD_livecoin = null;
+var currentPrice_BuyCOIN_USD_hotbit = null;
+var currentPrice_SellCOIN_USD_hotbit = null;
 
  
 
@@ -63,19 +63,15 @@ var currentPrice_SellPZM_USD_hotbit = null;
    fetch(resUrl1)
     .then(res => {
       res.json().then(body => {
-      divBuyPZM_USD.innerHTML = body.buy[0].price;  
-    divBuyPZM_USD1.innerHTML = body.buy[0].amount; 
-      divSellPZM_USD.innerHTML = body.sell[0].price;
-    divSellPZM_USD1.innerHTML = body.sell[0].amount;
+      divBuyCOIN_USD.innerHTML = body.buy[0].price;  
+    divBuyCOIN_USD1.innerHTML = body.buy[0].amount; 
+      divSellCOIN_USD.innerHTML = body.sell[0].price;
+    divSellCOIN_USD1.innerHTML = body.sell[0].amount;
     
-      currentPrice_BuyPZM_USD = body.buy[0].price;
-      currentPrice_SellPZM_USD = body.sell[0].price;
+      currentPrice_BuyCOIN_USD = body.buy[0].price;
+      currentPrice_SellCOIN_USD = body.sell[0].price;
     
-    btcalphalivecoin.innerHTML = (((currentPrice_BuyPZM_USD_livecoin*100)/currentPrice_SellPZM_USD)-100).toFixed(2); 
-   livecoinbtcalpha.innerHTML = (((currentPrice_BuyPZM_USD*100)/currentPrice_SellPZM_USD_livecoin)-100).toFixed(2); 
-livecoinhotbit.innerHTML = (((currentPrice_SellPZM_USD_hotbit*100)/currentPrice_SellPZM_USD_livecoin)-100).toFixed(2);             
- btcalphahotbit.innerHTML = (((currentPrice_SellPZM_USD_hotbit*100)/currentPrice_SellPZM_USD)-100).toFixed(2);        
-      console.log(body);
+    
    
     });
   })
@@ -87,23 +83,33 @@ livecoinhotbit.innerHTML = (((currentPrice_SellPZM_USD_hotbit*100)/currentPrice_
    fetch(resUrl2)
     .then(res => {
       res.json().then(body => {  
-      divBuyPZM_USD_livecoin.innerHTML = body.bids[0][0];
-    divBuyPZM_USD_livecoin1.innerHTML = body.bids[0][1];
-      divSellPZM_USD_livecoin.innerHTML = body.asks[0][0];
-    divSellPZM_USD_livecoin1.innerHTML = body.asks[0][1];
+      divBuyCOIN_USD_livecoin.innerHTML = body.bids[0][0];
+    divBuyCOIN_USD_livecoin1.innerHTML = body.bids[0][1];
+      divSellCOIN_USD_livecoin.innerHTML = body.asks[0][0];
+    divSellCOIN_USD_livecoin1.innerHTML = body.asks[0][1];
     
-       currentPrice_BuyPZM_USD_livecoin = body.bids[0][0];
-       currentPrice_SellPZM_USD_livecoin = body.asks[0][0];
-    
-  if(currentPrice_SellPZM_USD < currentPrice_BuyPZM_USD_livecoin && $('#btcalphalivecoin_notificator').prop('checked'))beep();
-        
-        
-     if(currentPrice_SellPZM_USD_livecoin < currentPrice_BuyPZM_USD && $('#livecoinbtcalpha_notificator').prop('checked'))beep();   
+       currentPrice_BuyCOIN_USD_livecoin = body.bids[0][0];
+       currentPrice_SellCOIN_USD_livecoin = body.asks[0][0];
+	      
+livecoinbtcalpha.innerHTML = (((currentPrice_BuyCOIN_USD*100)/currentPrice_SellCOIN_USD_livecoin)-100).toFixed(2);
+livecoinhotbit.innerHTML = (((currentPrice_SellCOIN_USD_hotbit*100)/currentPrice_SellCOIN_USD_livecoin)-100).toFixed(2);
+btcalphalivecoin.innerHTML = (((currentPrice_BuyCOIN_USD_livecoin*100)/currentPrice_SellCOIN_USD)-100).toFixed(2); 	      
+hotbitlivecoin.innerHTML = (((currentPrice_BuyCOIN_USD_livecoin*100)/currentPrice_BuyCOIN_USD_hotbit)-100).toFixed(2);
+btcalphahotbit.innerHTML = (((currentPrice_SellCOIN_USD_hotbit*100)/currentPrice_SellCOIN_USD)-100).toFixed(2);        
+      console.log(body);	      
+	      
+	      
+if(currentPrice_SellCOIN_USD_livecoin < currentPrice_BuyCOIN_USD && $('#livecoinbtcalpha_notificator').prop('checked'))beep();	      
+if(currentPrice_SellCOIN_USD_livecoin < currentPrice_SellCOIN_USD_hotbit && $('#livecoinhotbit_notificator').prop('checked'))beep();	      
+if(currentPrice_SellCOIN_USD < currentPrice_BuyCOIN_USD_livecoin && $('#btcalphalivecoin_notificator').prop('checked'))beep();
+if(currentPrice_BuyCOIN_USD_hotbit < currentPrice_BuyCOIN_USD_livecoin && $('#hotbitlivecoin_notificator').prop('checked'))beep();        
+if(currentPrice_SellCOIN_USD < currentPrice_SellCOIN_USD_hotbit && $('#btcalphahotbit_notificator').prop('checked'))beep();        
+if(currentPrice_BuyCOIN_USD_hotbit < currentPrice_BuyCOIN_USD && $('#hotbitbtcalpha_notificator').prop('checked'))beep();        
        
-    if(currentPrice_SellPZM_USD_livecoin < currentPrice_SellPZM_USD_hotbit && $('#livecoinhotbit_notificator').prop('checked'))beep();      
-     if(currentPrice_SellPZM_USD < currentPrice_SellPZM_USD_hotbit && $('#btcalphahotbit_notificator').prop('checked'))beep(); 
-      if(currentPrice_BuyPZM_USD_hotbit < currentPrice_BuyPZM_USD && $('#hotbitbtcalpha_notificator').prop('checked'))beep();    
-     if(currentPrice_BuyPZM_USD_hotbit < currentPrice_BuyPZM_USD_livecoin && $('#hotbitlivecoin_notificator').prop('checked'))beep();
+          
+      
+          
+     
         
      
     console.log(body);
@@ -117,13 +123,13 @@ livecoinhotbit.innerHTML = (((currentPrice_SellPZM_USD_hotbit*100)/currentPrice_
     .then(res => {
       res.json().then(body => {  
       
-    divBuyPZM_USD_hotbit.innerHTML = body.result.orders[0].price;
-    divBuyPZM_USD_hotbit1.innerHTML = body.result.orders[0].left;
+    divBuyCOIN_USD_hotbit.innerHTML = body.result.orders[0].price;
+    divBuyCOIN_USD_hotbit1.innerHTML = body.result.orders[0].left;
      
-    currentPrice_BuyPZM_USD_hotbit = body.result.orders[0].price;
+    currentPrice_BuyCOIN_USD_hotbit = body.result.orders[0].price;
      
-        hotbitbtcalpha.innerHTML = (((currentPrice_BuyPZM_USD*100)/currentPrice_BuyPZM_USD_hotbit)-100).toFixed(2); 
-        hotbitlivecoin.innerHTML = (((currentPrice_BuyPZM_USD_livecoin*100)/currentPrice_BuyPZM_USD_hotbit)-100).toFixed(2); 
+        hotbitbtcalpha.innerHTML = (((currentPrice_BuyCOIN_USD*100)/currentPrice_BuyCOIN_USD_hotbit)-100).toFixed(2); 
+         
       
         
        
@@ -137,10 +143,10 @@ livecoinhotbit.innerHTML = (((currentPrice_SellPZM_USD_hotbit*100)/currentPrice_
    fetch(resUrl4)
     .then(res => {
       res.json().then(body => {  
- 	 divSellPZM_USD_hotbit.innerHTML = body.result.orders[0].price;
-   divSellPZM_USD_hotbit1.innerHTML = body.result.orders[0].left;
+ 	 divSellCOIN_USD_hotbit.innerHTML = body.result.orders[0].price;
+   divSellCOIN_USD_hotbit1.innerHTML = body.result.orders[0].left;
     
-   currentPrice_SellPZM_USD_hotbit = body.result.orders[0].price;
+   currentPrice_SellCOIN_USD_hotbit = body.result.orders[0].price;
             
     console.log(body);
 
@@ -156,41 +162,4 @@ livecoinhotbit.innerHTML = (((currentPrice_SellPZM_USD_hotbit*100)/currentPrice_
   function beep() {
 	(new Audio("https://adolf.ucoz.net/beep.wav")).play();
 };
-   
- 
-
-
-
- 
- function moneyConverter(valNum) {
-   
-    document.getElementById("inputdrub").innerHTML=((((valNum/currentPrice_SellPZM_USD)*currentPrice_BuyPZM_BTC)*currentPriceSell)-(((((valNum/currentPrice_SellPZM_USD)*currentPrice_BuyPZM_BTC)*currentPriceSell)*0.3)/100)).toFixed(2); 
-    
- }
-
-
-
-function readMore() {
- var dots = document.getElementById("dots");
- var more = document.getElementById("more");
- var bun = document.getElementById("bun");
-   
-	
-  	
-	
- if(dots.style.display === "none") {
-    dots.style.display = "inline";
-    bun.innerHTML = "Подробнее";
-    more.style.display = "none";
- } else {
-   dots.style.display = "none";
-   bun.innerHTML = "Скрыть";
-   more.style.display = "inline"; 
- }
- }	
-	
-
-
-
-
-
+  
