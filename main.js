c50cb136e644fa326cb8d009e92829450d70a168
ketlller <oculusvrdk2@gmail.com>
2019-12-15 20:02:57 +0200

    Add - покажет последнее время синхронизации

--- main.js
+++ main.js
@@ -619,6 +619,20 @@ if(currentPrice_BuyCOIN_USD_hotbit < currentPrice_BuyCOIN_USD && $('#hotbitbtcal
   .catch(err => console.log(err)); 
   
    
+	 
+  // Last Sync: 15/12/2019 @ 19:52:37 - последний запуск функции покажет)),  обновили и показало(обновило) время и на паузу его поставило
+   var currentdate = new Date(); 
+	var datetime = "Last Sync: " + currentdate.getDate() + "/"
+	                + (currentdate.getMonth()+1)  + "/" 
+	                + currentdate.getFullYear() + " @ "  
+	                + currentdate.getHours() + ":"  
+	                + currentdate.getMinutes() + ":" 
+	                + currentdate.getSeconds();
+	$('.updtime span').html(datetime);
+  
+   });	 
+	 
+	 
    
    setTimeout(() => {   
 //отправляем смс на Телеграм бота   
@@ -647,8 +661,6 @@ livecoinbtcalphaSMS(token,text1,text2,text3,text4,text5,text6,chatid);
    
    
    
-  
-   });
 
 
 
